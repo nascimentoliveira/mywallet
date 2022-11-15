cd10f3707eb6aadfe05278d6230a1bc19b1e7ab5
Thiago Nascimento <nascimentoliveira@gmail.com>
2022-11-15 17:56:52 -0300

    feat (Sign In): static sign in page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import GlobalStyle from './assets/styles/GlobalStyle';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SignInPage from './pages/SignInPage';
 
 export default function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Routes>
-       {/*  <Route path='/' element={<SignInPage />} />
-        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/' element={<SignInPage />} />
+        {/* <Route path='/signup' element={<SignUpPage />} />
         <Route path='/wallet' element={<WalletPage />} />
         <Route path='/inflow' element={<InflowPage />} />
         <Route path='/outflow' element={<OutflowPage />} /> */}
---
+++ src/pages/SignInPage.js
@@ -0,0 +1,144 @@
+import styled from 'styled-components';
+
+export default function SignInPage() {
+
+  return (
+    <PageContainer>
+      <Logo>MyWallet</Logo>
+      <Form>
+        <Input
+          type='email'
+          placeholder='E-mail'
+          name='email'
+          required
+        >
+        </Input>
+
+        <Input
+          type='password'
+          placeholder='Senha'
+          name='password'
+          required
+        >
+        </Input>
+
+        <Button type='submit'>
+          Entrar
+        </Button>
+      </Form>
+
+        <ButtonSwap>
+          Primeira vez? Cadastre-se!
+        </ButtonSwap>
+    </PageContainer>
+  );
+}
+
+const PageContainer = styled.main`
+  width: 100%;
+  height: 100vh;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+	background: linear-gradient(-45deg, #8C11BE, #6F00FF, #8C11BE, #6F00FF);
+	background-size: 400% 400%;
+  padding: 5%;
+  box-sizing: border-box;
+	animation: gradient 10s ease infinite;
+  transition: 2s;
+
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+`;
+
+const Logo = styled.span`
+  font-family: 'Saira Stencil One', sans-serif;
+  font-weight: 400;
+  font-size: 32px;
+  line-height: 50px;
+  margin: 24px 0px;
+  color: #FFFFFF;
+`;
+
+const Form = styled.form`
+  max-width: 330px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  height: 58px;
+  font-family: 'Raleway', sans-serif;
+  font-size: 20px;
+  line-height: 23px;
+  color: #000000;
+  background-color: #FFFFFF;
+  border-radius: 5px;
+  border: none;
+  outline: none;
+  padding: 0px 15px;
+  margin-bottom: 13px;
+  box-sizing: border-box;
+  
+  &::placeholder {
+    font-family: 'Raleway', sans-serif;
+    font-size: 20px;
+    line-height: 23px;
+    color: #000000;
+  }
+  &:focus {
+    outline: none;
+  }
+`;
+
+const Button = styled.button`
+  width: 100%;
+  height: 46px;
+  font-family: 'Lexend Deca', sans-serif;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 23px;
+  color: #FFFFFF;
+  background-color: #A328D6;
+  border-radius: 5px;
+  margin: 3px;
+  border: none;
+  outline: none;
+  opacity: .8;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+const ButtonSwap = styled.button`
+  font-family: 'Raleway';
+  font-weight: 700;
+  font-size: 15px;
+  line-height: 18px;
+  margin: 36px 0px;
+  box-sizing: border-box;
+  background-color: transparent;
+  color: #FFFFFF;
+  border: none;
+  outline: none;
+  opacity: .8;
+
+&:hover {
+  opacity: 1;
+}
+`;
